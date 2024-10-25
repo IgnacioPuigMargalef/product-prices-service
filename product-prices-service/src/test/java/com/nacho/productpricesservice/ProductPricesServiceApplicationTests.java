2bbad34160bbfd18579db2b16364284bbc7f7420
IgnacioPuigMargalef <nachopm21@gmail.com>
2024-10-25 23:50:08 +0200

    Se añade el proyecto base Spring Boot con las dependencias definidas y se elimina contenido innecesario en el .gitignore

---
+++ product-prices-service/src/test/java/com/nacho/productpricesservice/ProductPricesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nacho.productpricesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductPricesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
